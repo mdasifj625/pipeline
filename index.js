e0ad7ab1f11040c0538d39460038b51a80d33998
MD ASIF JAWED <mdasifj625@gmail.com>
2023-12-25 02:34:43 +0530

    added some sctipt

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
 		message: 'Welcome to the ec2 machine',
 	});
 });
+app.get('/time', (req, res) => {
+	res.send({
+		status: 'success',
+		message: new Date().toLocaleString(),
+	});
+});
 
 app.listen(3300, () => {
 	console.log('server is up 🤫');
