b7a6292f06296fc886359a54df04f1fe12f102bd
MD ASIF JAWED <mdasifj625@gmail.com>
2023-12-25 02:27:58 +0530

    added some sctipt

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -16,9 +16,20 @@ fi
 
 # Delete old and create a new Nginx configuration file for the application
 CONFIG_FILE="/etc/nginx/sites-available/$APP_NAME.conf"
-sudo rm $CONFIG_FILE
+
+# Delete old Nginx configuration file for the application if it exists
+if [ -f "$CONFIG_FILE" ]; then
+    sudo rm "$CONFIG_FILE"
+    echo "Old configuration file removed."
+fi
+
 sudo touch $CONFIG_FILE
-sudo systemctl restart nginx
+if sudo systemctl reload nginx; then
+    echo "Nginx configuration reloaded successfully."
+else
+    echo "Failed to reload Nginx. Check the configuration."
+    exit 1
+fi
 
 # Configure Nginx to forward port 80 to port $APP_PORT
 # echo "server {
@@ -38,7 +49,7 @@ sudo systemctl restart nginx
 # in case of https config
 
 # make sure ssl folder is available
-sudo mkdir /etc/nginx/ssl
+sudo mkdir -p /etc/nginx/ssl
 
 # Read the content of cert.pem and save it to $APP_HOST.pem
 sudo cat cert.pem | sudo tee /etc/nginx/ssl/$APP_HOST.pem >/dev/null
@@ -71,7 +82,7 @@ echo "server {
     server_name $APP_HOST;
 
     # Redirect HTTP to HTTPS
-    return 301 https://$server_name$request_uri;
+    return 301 https://\$server_name\$request_uri;
 }
 
 server {
@@ -85,22 +96,34 @@ server {
     location / {
         proxy_pass http://127.0.0.1:$APP_PORT;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
     }
 }" | sudo tee $CONFIG_FILE
 
-# Enable the configuration by creating a symlink
-sudo ln -s /etc/nginx/sites-available/$APP_NAME.conf /etc/nginx/sites-enabled/
+# Enable the configuration by creating a symlink if it doesn't exist
+if [ ! -f "/etc/nginx/sites-enabled/$APP_NAME.conf" ]; then
+    sudo ln -s "$CONFIG_FILE" "/etc/nginx/sites-enabled/"
+    echo "Symlink created for $APP_NAME configuration."
+else
+    echo "Symlink already exists for $APP_NAME configuration."
+fi
 
 # Check if there's a need to remove the default symlink
 if [ -L "/etc/nginx/sites-enabled/default" ]; then
     sudo rm /etc/nginx/sites-enabled/default
 fi
 
+# Test Nginx configuration
+sudo nginx -t
+
 # Reload Nginx to apply changes
-sudo systemctl restart nginx
-sudo systemctl reload nginx
+if sudo systemctl restart nginx; then
+    echo "Nginx service restarted successfully."
+else
+    echo "Failed to restart Nginx. Check the configuration."
+    exit 1
+fi
 echo "Nginx is configured to forward port 80 to port $APP_PORT for $APP_NAME."
